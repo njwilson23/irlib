b59974ff41129cf290a9290e9525528649aa1340
Nat Wilson <njwilson23@gmail.com>
2013-05-03 15:37:45 -0700

    change BRP window to be close to that used by Copland and Sharp (2001)

--- irlib/brp.py
+++ irlib/brp.py
@@ -26,7 +26,9 @@ def get_brp_windowed(L, relwin=(None, None)):
     """ Calculate the bed reflection power within a window relative to the
     picks in Gather `L`. """
     if None in relwin:
-        relwin = (int(round(-2e8*L.rate)), int(round(1e9*L.rate)))
+        #relwin = (int(round(-1e9*L.rate)), int(round(5e9*L.rate)))
+        # Use Copland and Sharp's window (100 ns prior, 400 ns after)
+        relwin = (int(round(-100e-9/L.rate)), int(round(400e-9/L.rate)))
     power = lambda a: np.sum(a**2) * L.rate
     bed_wavelets = map(lambda tr, i: extract_window_around(tr, i, relwin),
                        L.data.T, L.bed_picks)
@@ -40,7 +42,7 @@ def get_irp(L):
     air = L.dc_picks
     bed = L.bed_picks
     # Amount to avoid airwave/bedwave by, in samples:
-    pd = int(round(1e-7 / L.rate))
+    pd = int(round(100e-9 / L.rate)) # 100 ns
     internal_windows = [L.data[a+pd:b-pd] for a,b in zip(air,bed)]
     return map(power, internal_windows)
 
