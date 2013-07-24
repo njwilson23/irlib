f4fa5179edec29a30421f842053de8f1a562bf5e
Nat Wilson <njwilson23@gmail.com>
2013-07-24 11:42:19 -0500

    Revert "fix a number of obsolete variable names in refactors app/ code"
    accidentally committed twice
    
    This reverts commit fd22cd9dfa6daef98e53623c9cb9caec0f68efe9.

--- irlib/app/console.py
+++ irlib/app/console.py
@@ -38,8 +38,6 @@ class Console(object):
 
         self.open_line(lineno)
         self.appwindows.append(Radargram(self.line))
-        for rg in self.get_appwindows(Radargram):
-            rg.update()
 
         # Begin main loop
         print bannertext
