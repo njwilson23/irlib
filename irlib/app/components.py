njwilson23/irlib

commit 9c49f1acd182ed85ab635f490ed8502d65c0da32

Simple hack to fix issue #6
It seems that `self.fig.canvas.toolbar.destroy()` isn't safe for all possible
backends. A work-around is to dynamically set the rcParams, although this could
cause the effect to spread to other windows if used in a script with other
matplotlib calls.

It seems reasonable that `irlib.app` will be used stand-alone, so I did it for
now with the rcParams adjustment happening on load of the
`irlib.app.components.module`.

irlib/app/components.py
@@ -3,12 +3,13 @@
 import sys
 import os
 import irlib
-from irlib.misc import TryCache
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-import traceback
+from irlib.misc import TryCache
 
 plt.ion()
+matplotlib.rcParams["toolbar"] = "none"
 
 class AppWindow(object):
     """ This is the generic application window class, and contains an axes
@@ -23,7 +24,6 @@ class AppWindow(object):
         winsize : size of the window, in inches
         """
         self.fig = plt.figure(figsize=winsize)
-        self.fig.canvas.toolbar.destroy()
         self.ax = self.fig.add_subplot(1,1,1)
 
         # Turn off default shortcuts
@@ -336,7 +336,6 @@ class Radargram(AppWindow):
                         y = float(slist[3])
                         pnt_list.append((x, y))
                     except:
-                        traceback.print_exc()
                         sys.stdout.write("Failed to read record:\n\t{0}".format(s))
 
             if len(pnt_list) == 0:
@@ -713,6 +712,7 @@ class MapWindow(AppWindow):
     def __init__(self, L):
         super(MapWindow, self).__init__((4, 4))
         self._newline(L)
+        self.ax.tight_layout()
         return
 
     def _newline(self, L):

commit 54170d18228e964250409ffc14ec8e831b4d9b55

fix MapWindow regression introduced by 9c49f1acd182ed85ab635f490ed8502d65c0da32

irlib/app/components.py
@@ -712,7 +712,7 @@ class MapWindow(AppWindow):
     def __init__(self, L):
         super(MapWindow, self).__init__((4, 4))
         self._newline(L)
-        self.ax.tight_layout()
+        self.fig.tight_layout()
         return
 
     def _newline(self, L):