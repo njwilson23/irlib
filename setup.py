a9bd9a81735fbbe936745ab1ded1c6380e3d58ab
Nat Wilson <njwilson23@gmail.com>
2013-11-16 21:36:03 -0500

    add version to setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
-#from Cython.Distutils import build_ext
 from Cython.Build import cythonize
-
-# Run with `python setup.py build_ext --inplace`
 
 ext_modules = [Extension("irlib.agc", ["irlib/agc.pyx"])]
 ext_modules = cythonize(ext_modules)
@@ -11,9 +8,9 @@ ext_modules = cythonize(ext_modules)
 setup(
     name = "irlib",
     author = "Nat Wilson",
+    version = "0.4",
     packages = ["irlib"],
     ext_modules = ext_modules,
-    #cmdclass = {'build_ext': build_ext},
     scripts = ["icepick2.py", "icerate.py", "h5_add_utm.py", "h5_dumpmeta.py", \
                "h5_replace_gps.py", "h5_generate_caches.py", "h5_consolidate.py", \
                "h52mat.py"],
