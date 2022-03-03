f985a6361f3c33a639ca1ba59127fcb0c417dcbc
Derek Mueller <derek.mueller@carleton.ca>
2022-03-03 08:19:45 -0500

    Update setup.py
    Changed version number and added more utiliites

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ except ImportError:
 setup(
     name = "irlib",
     author = "Nat Wilson",
-    version = "0.4.3",
+    version = "0.5",
     packages = ["irlib"],
     ext_modules = ext_modules,
     scripts = ["icepick2.py", "icerate.py", "h5_add_utm.py", "h5_dumpmeta.py", \
                "h5_replace_gps.py", "h5_generate_caches.py", "h5_consolidate.py", \
-               "h52mat.py"],
+               "h5_export.py", "h5_mat.py", "irview.py","join_radar.py", \
+               "mergepicks.py","plotline.py", "plottrace.py"],
 )
